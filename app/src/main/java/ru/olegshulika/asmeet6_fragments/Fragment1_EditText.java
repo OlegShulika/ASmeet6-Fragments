3f7de3a606c36e26a39f154e686257bc36617d6d
OlegShulika <olegshulika@gmail.com>
2018-12-24 20:48:59 +0300

    1st ver

---
+++ app/src/main/java/ru/olegshulika/asmeet6_fragments/Fragment1_EditText.java
@@ -0,0 +1,25 @@
+package ru.olegshulika.asmeet6_fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment1_EditText extends Fragment {
+
+    public Fragment1_EditText() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment1__edittext, container, false);
+    }
+
+}
